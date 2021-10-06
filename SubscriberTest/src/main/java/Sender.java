e72a743da6321544fd96fc89bf243551c56c3dfc
monokeith <hgyixg@gmail.com>
2021-10-06 00:56:43 -0400

    Example code: send message

---
+++ SubscriberTest/src/main/java/Sender.java
@@ -0,0 +1,31 @@
+import com.solacesystems.jms.SolConnectionFactory;
+import com.solacesystems.jms.SolJmsUtility;
+
+import javax.jms.*;
+import java.util.concurrent.CountDownLatch;
+
+public class Sender {
+
+    public static void main(String[] args) throws Exception {
+        // Setup
+        SolConnectionFactory connectionFactory = SolJmsUtility.createConnectionFactory();
+        connectionFactory.setHost("tcp://localhost:55555");
+        connectionFactory.setUsername("default");
+        connectionFactory.setPassword("default");
+        connectionFactory.setVPN("LightBot");
+        Connection connection = connectionFactory.createConnection();
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+
+        // Send
+        final String TOPIC_NAME = "try-me";
+        Topic topic = session.createTopic(TOPIC_NAME);
+        MessageProducer messageProducer = session.createProducer(topic);
+
+        TextMessage message = session.createTextMessage("Hello world!");
+        messageProducer.send(topic, message, DeliveryMode.NON_PERSISTENT,
+                Message.DEFAULT_PRIORITY, Message.DEFAULT_TIME_TO_LIVE);
+
+    }
+
+}
--- SubscriberTest/src/main/java/Subscriber.java
+++ SubscriberTest/src/main/java/Subscriber.java
@@ -1,4 +1,3 @@
-
 import com.solacesystems.jms.SolConnectionFactory;
 import com.solacesystems.jms.SolJmsUtility;
 
@@ -13,7 +12,7 @@ public class Subscriber {
         connectionFactory.setHost("tcp://localhost:55555");
         connectionFactory.setUsername("default");
         connectionFactory.setPassword("default");
-        connectionFactory.setVPN("default");
+        connectionFactory.setVPN("LightBot");
         Connection connection = connectionFactory.createConnection();
         Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
 
@@ -22,16 +21,18 @@ public class Subscriber {
         final String TOPIC_NAME = "try-me";
         Topic topic = session.createTopic(TOPIC_NAME);
         MessageConsumer messageConsumer = session.createConsumer(topic);
-        final CountDownLatch latch = new CountDownLatch(1);
+        final CountDownLatch latch = new CountDownLatch(3);
+
         messageConsumer.setMessageListener(message -> {
             try {
                 if (message instanceof TextMessage) {
                     System.out.printf("TextMessage received: '%s'%n", ((TextMessage) message).getText());
                 } else {
                     System.out.println("Message received.");
+                    System.out.printf("Message Content:%n%s%n", SolJmsUtility.dumpMessage(message));
                 }
-                System.out.printf("Message Content:%n%s%n", SolJmsUtility.dumpMessage(message));
-                latch.countDown(); // unblock the main thread
+
+                //latch.countDown(); // unblock the main thread
             } catch (JMSException ex) {
                 System.out.println("Error processing incoming message.");
                 ex.printStackTrace();
